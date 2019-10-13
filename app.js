f5a3d9082cc98ce7fb35c3162575afd0418719ce
flopown <thijsbenjamin@live.be>
2019-10-14 01:30:00 +0200

    Starting the change to OOP

--- app.js
+++ app.js
@@ -1,3 +1,119 @@
+class Piece {
+    constructor(color, position, containing_element) {
+        this.color = color;
+        this.position = position;
+
+        this.containing_element = containing_element;
+
+        this.background_color = this.containing_element.style.background;
+        this.selected = false;
+
+        // // Make the class abstract
+        // if (this.constructor === Piece) {
+        //     throw new TypeError('Abstract class "Piece" cannot be instantiated directly.'); 
+        // }
+
+        // // No interfaces in javascript so this will have to do
+        // if (this.move === undefined || typeof(this.move) !== "function") {
+        //     throw new TypeError('Classes extending the piece abstract class need to implement the move function.');
+        // }
+
+        // if (this.available_moves === undefined || typeof(this.available_moves) !== "function") {
+        //     throw new TypeError('Classes extending the piece abstract class need to implement the available_moves function.');
+        // }
+    }
+
+    select() {
+        if (this.selected) {
+            this.unselect();
+            return;
+        }
+
+        // Unselect all the pieces first
+        for (let i = 0; i < pieces.length; i++) {
+            pieces[i].unselect();
+        }
+        
+        console.log(
+            {
+                'piece': this.color + ' ' + this.constructor.name,
+                'position': this.position
+            });
+
+        this.containing_element.style.background = 'red';
+        this.selected = true;
+    }
+
+    unselect() {
+        if (!this.selected) return;
+
+        this.containing_element.style.background = this.background_color;
+        this.selected = false;
+    }
+}
+
+class Rook extends Piece {
+    available_moves() {
+        // Get the available moves
+    }
+
+    move(position) {
+        // Move to a position
+    }
+}
+
+class Knight extends Piece {
+    available_moves() {
+        // Get the available moves
+    }
+
+    move(position) {
+        // Move to a position
+    }
+}
+
+class Bishop extends Piece {
+    available_moves() {
+        // Get the available moves
+    }
+
+    move(position) {
+        // Move to a position
+    }
+}
+
+class Queen extends Piece {
+    available_moves() {
+        // Get the available moves
+    }
+
+    move(position) {
+        // Move to a position
+    }
+}
+
+class King extends Piece {
+    available_moves() {
+        // Get the available moves
+    }
+
+    move(position) {
+        // Move to a position
+    }
+}
+
+class Pawn extends Piece {
+    available_moves() {
+        // Get the available moves
+    }
+
+    move(position) {
+        // Move to a position
+    }
+}
+
+pieces = []
+
 function setPiece(piece, position) {
     let img = document.createElement('img');
     img.src = "resources/" +  piece + ".png";
@@ -9,12 +125,13 @@ function setPiece(piece, position) {
     let col = row.getElementsByClassName(position.substring(0,1))[0];
 
     col.firstChild.appendChild(img);
-}
 
-function clearSquare(position) {
-    let row = document.getElementsByClassName(position.substring(1,2))[0];
-    let col = row.getElementsByClassName(position.substring(0,1))[0];
-    col.firstChild.innerHTML = '';
+    piece = new Piece(piece.split('-')[0], position, col.firstChild);
+    pieces.push(piece);
+
+    img.onclick = function() {
+        piece.select();
+    };
 }
 
 // TODO: add input to choose color you want to play
@@ -25,10 +142,10 @@ function setInitialState(color) {
     // Top main pieces
     setPiece(top + '-rook', 'A8');
     setPiece(top + '-knight', 'B8');
-    setPiece(top + '-rook', 'C8');
+    setPiece(top + '-bishop', 'C8');
     setPiece(top + '-queen', 'D8');
     setPiece(top + '-king', 'E8');
-    setPiece(top + '-rook', 'F8');
+    setPiece(top + '-bishop', 'F8');
     setPiece(top + '-knight', 'G8');
     setPiece(top + '-rook', 'H8');
 
@@ -40,10 +157,10 @@ function setInitialState(color) {
     // Bot main pieces
     setPiece(bot + '-rook', 'A1');
     setPiece(bot + '-knight', 'B1');
-    setPiece(bot + '-rook', 'C1');
+    setPiece(bot + '-bishop', 'C1');
     setPiece(bot + '-king', 'D1');
     setPiece(bot + '-queen', 'E1');
-    setPiece(bot + '-rook', 'F1');
+    setPiece(bot + '-bishop', 'F1');
     setPiece(bot + '-knight', 'G1');
     setPiece(bot + '-rook', 'H1');
 
@@ -56,9 +173,15 @@ function setInitialState(color) {
 function clearBoard() {
     for (let i = 1; i < 9; i++) {
         for (let j = 1; j < 9; j++) {
-            clearSquare(String.fromCharCode(64 + i) + j);
+            position = String.fromCharCode(64 + i) + j;
+
+            let row = document.getElementsByClassName(position.substring(1,2))[0];
+            let col = row.getElementsByClassName(position.substring(0,1))[0];
+            col.firstChild.innerHTML = '';
         }
     }
+
+    pieces = [];
 }
 
 setInitialState('white');
