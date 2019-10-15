60e9b3235de8c7be9d3a000bc284df0d218e48f5
flopown <thijsbenjamin@live.be>
2019-10-15 21:11:43 +0200

    I need to stop working in one file, but I don't know how

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # chess-js
- Just chess in Javascript.
+
+Just chess in Javascript.
+Now with actuall classes :)
+
+## FAQ (but not really)
+
+### Why did I decide to make this?
+
+Because I can. (And because I needed a js-project for school)
+
+### What will it be used for?
+
+Mainly to play against an AI.
+
+### What are the AI specs?
+
+Non-existant at the moment :)
+I will just use some pre-made AI-model once the actual engine is working.
+Eventually I will make my own AI, but that will still take some time most likely.
+
+### Will you take this seriously?
+
+Hell no.
+
+### Will we be able to ask help when it breaks?
+
+Yes, if you give me money :)
+
+### Are we allowed to use this project?
+
+Yes, but only for educational purposes.
+Jk, do whatever you want, I'm not your mom.
+
+### Hotel?
+
+Trivago.
--- app.js
+++ app.js
@@ -1,54 +1,271 @@
-class Piece {
-    constructor(color, position, containing_element) {
-        this.color = color;
+class ChessBoard {
+    constructor(table) {
+        this.table = table;
+
+        this.squares = [];
+        this.pieces = [];
+
+        this.primary_color = 'white';
+
+        this.set_squares();
+        this.setInitialState();
+    }
+
+    unselect_all_pieces() {
+        this.pieces.forEach(piece => piece.unselect());
+    }
+
+    get_square_by_name(name) {
+        // We're assuming this never goes wrong, otherwise we're ducked
+        return this.squares.filter(square => square.name === name)[0];
+    }
+
+    set_squares() {
+        // JS-scopes are bs
+        let self = this;
+
+        // Yeh, table.children returns the tbody, but table has no firstChild... JS? more like BS.
+        let rows = this.table.children[0].children;
+
+        // Foreach loops are nice, until you get into scopes :/
+        Array.from(rows).forEach(function(row) {
+            let row_num = row.className;
+
+            // JS-scopes are still bs
+            let _this = self;
+
+            Array.from(row.children).forEach(function(col) {
+                let col_char = col.className;
+                let position = {'x': col_char.charCodeAt(0), 'y': row_num};
+                let div = col.firstChild;
+
+                _this.squares.push(new Square(col_char+row_num, position, div));
+            });
+        });
+
+        // Do we want this? not sure
+        // Do we want the other one? idk
+        // Hotel? Trivago
+
+        // for (let i = 0; i < rows.length; i++) {
+        //     let row_num = rows[i].className;
+        //     let cols = rows[i].children;
+
+        //     for (let j = 0; j < cols.length; j++) {
+        //         let col_char = cols[j].className;
+        //         let position = {'x': col_char.charCodeAt(0), 'y': row_num};
+        //         let div = cols[j].firstChild;
+
+        //         this.squares.push(new Square(col_char+row_num, position, div));
+        //     }
+        // }
+    }
+
+    setPiece(piece_name, position, player_number) {
+        let img = document.createElement('img');
+        img.src = "resources/" +  piece_name + ".png";
+        img.alt = piece_name;
+    
+        let square = this.get_square_by_name(position);
+
+        square.element.appendChild(img);
+    
+        let piece_color = piece_name.split('-')[0];
+        let piece_type = piece_name.split('-')[1];
+
+        let piece;
+    
+        // Find a better way to do this
+        switch (piece_type) {
+            case 'rook':
+                piece = new Rook(this, piece_color, square, player_number);
+                break;
+            case 'knight':
+                piece = new Knight(this, piece_color, square, player_number);
+                break;
+            case 'bishop':
+                piece = new Bishop(this, piece_color, square, player_number);
+                break;
+            case 'queen':
+                piece = new Queen(this, piece_color, square, player_number);
+                break;
+            case 'king':
+                piece = new King(this, piece_color, square, player_number);
+                break;
+            case 'pawn':
+                piece = new Pawn(this, piece_color, square, player_number);
+                break;
+            default:
+                piece = new Piece(this, piece_color, square, player_number);
+                break; 
+        }
+        
+        this.pieces.push(piece);
+    }
+    
+    setInitialState() {
+        let top = this.primary_color.toLowerCase() === 'black' ? 'white' : 'black';
+        let top_player_number = top === 'white' ? 1 : 2;
+
+        let bot = this.primary_color.toLowerCase() === 'black' ? 'black' : 'white';
+        let bot_player_number = bot === 'white' ? 1 : 2;
+
+        // Top main pieces
+        this.setPiece(top + '-rook', 'A8', top_player_number);
+        this.setPiece(top + '-knight', 'B8', top_player_number);
+        this.setPiece(top + '-bishop', 'C8', top_player_number);
+        this.setPiece(top + '-queen', 'D8', top_player_number);
+        this.setPiece(top + '-king', 'E8', top_player_number);
+        this.setPiece(top + '-bishop', 'F8', top_player_number);
+        this.setPiece(top + '-knight', 'G8', top_player_number);
+        this.setPiece(top + '-rook', 'H8', top_player_number);
+
+        // Top pawns
+        for (let i = 8; i > 0; i--) {
+            this.setPiece(top + '-pawn', String.fromCharCode(64 + i) + '7', top_player_number);
+        }
+
+        // Bot main pieces
+        this.setPiece(bot + '-rook', 'A1', bot_player_number);
+        this.setPiece(bot + '-knight', 'B1', bot_player_number);
+        this.setPiece(bot + '-bishop', 'C1', bot_player_number);
+        this.setPiece(bot + '-king', 'D1', bot_player_number);
+        this.setPiece(bot + '-queen', 'E1', bot_player_number);
+        this.setPiece(bot + '-bishop', 'F1', bot_player_number);
+        this.setPiece(bot + '-knight', 'G1', bot_player_number);
+        this.setPiece(bot + '-rook', 'H1', bot_player_number);
+
+        // Bot pawns
+        for (let i = 8; i > 0; i--) {
+            this.setPiece(bot + '-pawn', String.fromCharCode(64 + i) + '2', bot_player_number);
+        }
+    }
+    
+    clearBoard() {
+        for (let i = 1; i < 9; i++) {
+            for (let j = 1; j < 9; j++) {
+                let position = String.fromCharCode(64 + i) + j;
+    
+                let row = document.getElementsByClassName(position.substring(1,2))[0];
+                let col = row.getElementsByClassName(position.substring(0,1))[0];
+                col.firstChild.innerHTML = '';
+            }
+        }
+    
+        this.pieces = [];
+    }
+}
+
+class Square {
+    constructor(name, position, element) {
+        // Name eg. B4, E2, H6, ...
+        this.name = name;
+
+        // Position is a dictionary with values 'x' and 'y', (0,0) is the bottom-left of the grid
         this.position = position;
+        this.element = element;
+        
+        this.base_background = this.element.style.background;
+        this.selected_color = 'red';
+        this.available_move_color = 'green';
 
-        this.containing_element = containing_element;
-
-        this.background_color = this.containing_element.style.background;
         this.selected = false;
-
-        // // Make the class abstract
-        // if (this.constructor === Piece) {
-        //     throw new TypeError('Abstract class "Piece" cannot be instantiated directly.'); 
-        // }
-
-        // // No interfaces in javascript so this will have to do
-        // if (this.move === undefined || typeof(this.move) !== "function") {
-        //     throw new TypeError('Classes extending the piece abstract class need to implement the move function.');
-        // }
-
-        // if (this.available_moves === undefined || typeof(this.available_moves) !== "function") {
-        //     throw new TypeError('Classes extending the piece abstract class need to implement the available_moves function.');
-        // }
     }
 
     select() {
-        if (this.selected) {
-            this.unselect();
-            return;
-        }
-
-        // Unselect all the pieces first
-        for (let i = 0; i < pieces.length; i++) {
-            pieces[i].unselect();
-        }
-        
-        console.log(
-            {
-                'piece': this.color + ' ' + this.constructor.name,
-                'position': this.position
-            });
-
-        this.containing_element.style.background = 'red';
+        this.show_as_selected();
         this.selected = true;
     }
 
-    unselect() {
-        if (!this.selected) return;
+    show_as_selected() {
+        this.element.style.background = this.selected_color;
+        this.selected = true;
+    }
 
-        this.containing_element.style.background = this.background_color;
+    show_as_available_move() {
+        this.element.style.background = this.available_move_color;
+    }
+
+    reset_color() {
+        this.element.style.background = this.base_background;
         this.selected = false;
+    }
+}
+
+class Piece {
+    constructor(chessBoard, color, square, player_number) {
+        // We need this to have access to the other pieces and squares
+        this.chessBoard = chessBoard;
+
+        this.color = color;
+        this.square = square;
+
+        // Which player controls the piece (player 1 or player 2)
+        this.player_number = player_number;
+
+        this.available_moves = [];
+
+        // Make the class abstract
+        if (this.constructor === Piece) {
+            throw new TypeError('Abstract class "Piece" cannot be instantiated directly.'); 
+        }
+
+        this.square.element.onclick = _ => this.select();
+    }
+
+    select() {
+        if (this.square.selected) {
+            this.square.reset_color();
+            return;
+        }
+        
+        this.chessBoard.unselect_all_pieces();
+        
+        // Select this piece (actually the square)
+        this.square.select();
+
+        // DEBUG
+        console.log(
+            {
+                'color': this.color,
+                'square': this.square.name,
+                'player': this.player_number
+            });
+
+        // Show them to the user
+        // this.show_available_moves();
+    }
+
+    unselect() {
+        this.square.reset_color();
+
+        // this.available_moves.forEach(function(square) {
+        //     square.reset_color();
+        // });
+    }
+
+    // It is assumed this will be implemented
+    move() {
+        this.available_moves = [];
+        this.unselect();
+
+        // TODO: do the rest
+    }
+
+    // It is assumed this will be implemented
+    get_available_moves() {
+        throw new TypeError('Classes extending the Piece abstract class need to implement the available_moves function.');
+    }
+
+    show_available_moves() {
+        // Get the available moves, not needed if we haven't moved since last time we got them
+        // So we need to reset it everytime we move
+        if (this.available_moves === []) {
+            this.available_moves = this.get_available_moves();
+        }
+
+        // The actual showing of the available squares
+        this.available_moves.forEach(square => square.show_as_available_move());
     }
 }
 
@@ -112,85 +329,15 @@ class Pawn extends Piece {
     }
 }
 
-pieces = []
-
-function setPiece(piece, position) {
-    let img = document.createElement('img');
-    img.src = "resources/" +  piece + ".png";
-
-    // Easiest way to keep track of positions
-    img.alt = piece + " " + position;
-
-    let row = document.getElementsByClassName(position.substring(1,2))[0];
-    let col = row.getElementsByClassName(position.substring(0,1))[0];
-
-    col.firstChild.appendChild(img);
-
-    piece = new Piece(piece.split('-')[0], position, col.firstChild);
-    pieces.push(piece);
-
-    img.onclick = function() {
-        piece.select();
-    };
-}
-
-// TODO: add input to choose color you want to play
-function setInitialState(color) {
-    let top = color.toLowerCase() === 'black' ? 'white' : 'black';
-    let bot = color.toLowerCase() === 'black' ? 'black' : 'white';
-
-    // Top main pieces
-    setPiece(top + '-rook', 'A8');
-    setPiece(top + '-knight', 'B8');
-    setPiece(top + '-bishop', 'C8');
-    setPiece(top + '-queen', 'D8');
-    setPiece(top + '-king', 'E8');
-    setPiece(top + '-bishop', 'F8');
-    setPiece(top + '-knight', 'G8');
-    setPiece(top + '-rook', 'H8');
-
-    // Top pawns
-    for (let i = 8; i > 0; i--) {
-        setPiece(top + '-pawn', String.fromCharCode(64 + i) + '7');
-    }
-
-    // Bot main pieces
-    setPiece(bot + '-rook', 'A1');
-    setPiece(bot + '-knight', 'B1');
-    setPiece(bot + '-bishop', 'C1');
-    setPiece(bot + '-king', 'D1');
-    setPiece(bot + '-queen', 'E1');
-    setPiece(bot + '-bishop', 'F1');
-    setPiece(bot + '-knight', 'G1');
-    setPiece(bot + '-rook', 'H1');
-
-    // Bot pawns
-    for (let i = 8; i > 0; i--) {
-        setPiece(bot + '-pawn', String.fromCharCode(64 + i) + '2');
-    }
-}
-
-function clearBoard() {
-    for (let i = 1; i < 9; i++) {
-        for (let j = 1; j < 9; j++) {
-            position = String.fromCharCode(64 + i) + j;
-
-            let row = document.getElementsByClassName(position.substring(1,2))[0];
-            let col = row.getElementsByClassName(position.substring(0,1))[0];
-            col.firstChild.innerHTML = '';
-        }
-    }
-
-    pieces = [];
-}
-
-setInitialState('white');
+chessBoard = new ChessBoard(document.getElementById('chess-board'));
 
 // Controls
 document.getElementById('reset-button').onclick = function(){
-    clearBoard();
+    chessBoard.clearBoard();
 
     let c = document.getElementById('color-selector');
-    setInitialState(c.options[c.selectedIndex].value);
+
+    chessBoard.primary_color = c.options[c.selectedIndex].value;
+    chessBoard.setInitialState();
 };
-document.getElementById('clear-button').onclick = clearBoard;
+document.getElementById('clear-button').onclick = chessBoard.clearBoard;
